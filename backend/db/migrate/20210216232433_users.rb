494aecfdcdb03ed7282255d0fd5bb3e5ab5d4675
kam3r0nz <kam.hungerford@gmail.com>
2021-02-16 17:41:37 -0600

    Added email column to users table

--- backend/db/migrate/20210216232433_users.rb
+++ backend/db/migrate/20210216232433_users.rb
@@ -2,6 +2,7 @@ class Users < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :email
     end
   end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_232528) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
   end
 
 end
