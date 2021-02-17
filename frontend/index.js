49251e9be3b4c4b2ef7af6a991d425f18f87c2f4
kam3r0nz <kam.hungerford@gmail.com>
2021-02-17 11:52:38 -0600

    Linked css file and added some styling and enter link

--- frontend/index.js
+++ frontend/index.js
@@ -4,4 +4,11 @@ window.addEventListener('DOMContentLoaded', e => {
     fetch('http://localhost:3000/songs')
         .then(resp => resp.json())
         .then(data => console.log(data))
+
+
+    const enterLink = document.getElementById('enter')
+    enterLink.addEventListener('click', e => {
+        e.preventDefault()
+        console.log('this has been clicked')
+    })
 })
