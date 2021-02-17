e2238ffecc81ea46a4ca2ec49a36bf5fe8ca80ff
kam3r0nz <kam.hungerford@gmail.com>
2021-02-17 12:31:46 -0600

    Removed some js and moved fetch call into seperate function

--- frontend/index.js
+++ frontend/index.js
@@ -1,20 +1,10 @@
 window.addEventListener('DOMContentLoaded', e => {
     console.log('Window loaded')
+    fetchSongs()
+})
 
+function fetchSongs() {
     fetch('http://localhost:3000/api/v1/songs')
         .then(resp => resp.json())
         .then(data => console.log(data))
-
-
-    
-    const enterLink = document.getElementById('enter')
-    enterLink.addEventListener('click', e => {
-        e.preventDefault()
-        console.log('this has been clicked')
-        const container = document.getElementById('container')
-        const newForm = document.createElement('form')
-        newForm.className = 'login fade-in'
-        newForm.innerHTML = '<input type="text" id="name" placeholder="Name"> <input type="email" id="email" placeholder="Email"> <input type="submit" value="Enter">'
-        container.appendChild(newForm)
-    })
-})
+}
