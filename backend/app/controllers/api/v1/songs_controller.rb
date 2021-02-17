b4ab93b8002357ec261defac737eec6b32e93a72
kam3r0nz <kam.hungerford@gmail.com>
2021-02-17 12:29:00 -0600

    Added create action in songs controller

--- backend/app/controllers/api/v1/songs_controller.rb
+++ backend/app/controllers/api/v1/songs_controller.rb
@@ -4,4 +4,15 @@ class Api::V1::SongsController < ApplicationController
         render json: songs
     end
 
+    def create
+        song = Song.create(song_params)
+        render json: song
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:name, :artist, :album, :album_cover, :user_id)
+    end
+
 end
