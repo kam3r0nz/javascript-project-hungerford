5c3dd47fa5957797b6dd6ef3e4ee2e7cbeadfeee
kam3r0nz <kam.hungerford@gmail.com>
2021-02-17 12:02:53 -0600

    Added arrow image that creates a form when clicked

--- frontend/index.js
+++ frontend/index.js
@@ -6,9 +6,15 @@ window.addEventListener('DOMContentLoaded', e => {
         .then(data => console.log(data))
 
 
+    
     const enterLink = document.getElementById('enter')
     enterLink.addEventListener('click', e => {
         e.preventDefault()
         console.log('this has been clicked')
+        const container = document.getElementById('container')
+        const newForm = document.createElement('form')
+        newForm.className = 'login'
+        newForm.innerHTML = '<input type="text"><br><input type="submit">'
+        container.appendChild(newForm)
     })
 })
