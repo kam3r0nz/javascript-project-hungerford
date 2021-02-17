ac1193bbbcf3630105f79955892eda5bcf127d4a
kam3r0nz <kam.hungerford@gmail.com>
2021-02-17 13:02:14 -0600

    Added simple fetch post request to create new song

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', e => {
     console.log('Window loaded')
     fetchSongs()
+    mountFormListener()
 
     const title = document.querySelector('#title')
     const artist = document.querySelector('#artist')
     const album = document.querySelector('#album')
-    const albumCover = document.querySelector('#album-cover')
+    const cover = document.querySelector('#cover')
     const submit = document.querySelector('#submit')
 })
 
@@ -30,6 +31,18 @@ const getSongData = function(form) {
         title: title.value,
         artist: artist.value,
         album: album.value,
-        albumCover: albumCover.value
+        albumCover: cover.value
     }
+}
+
+function createSong(songObj) {
+    fetch('http://localhost:3000/api/v1/songs', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({song: songObj})
+    })
+        .then(resp => resp.json())
+        .then(data => console.log(data))
 }
