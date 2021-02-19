640584e9281a9372afb99f15b3d12d3f6edceba2
kam3r0nz <kam.hungerford@gmail.com>
2021-02-18 18:56:22 -0600

    Fixed song form and mountFormListener function

--- frontend/index.js
+++ frontend/index.js
@@ -20,9 +20,27 @@ function addSongButton() {
     let songButton = document.createElement('button')
     songButton.innerHTML = 'Add Song'
     body.appendChild(songButton)
-    songButton.addEventListener('click',function gotClicked(e) {
+    songButton.addEventListener('click', e => {
         e.preventDefault()
         showSongForm()
         songButton.remove()
     })
+}
+
+function showSongForm() {
+    let body = document.getElementById('container')
+    let songFormContainer = document.createElement('div')
+    songFormContainer.setAttribute('id', 'song-form-container')
+    body.append(songFormContainer)
+    let songForm = document.createElement('form')
+    songForm.setAttribute('id', 'song-form')
+    songForm.setAttribute('class', 'fade-in')
+    songForm.innerHTML = '<input type="text" name="title" id="title" placeholder="Title">  <input type="text" name="artist" id="artist" placeholder="Artist"> <br> <input type="text" name="album" id="album" placeholder="Album">  <input type="text" name="album_cover" id="album_cover" placeholder="Album cover"> <br> <input type="submit" id="submit" value="Save">'
+    songFormContainer.append(songForm)
+    mountFormListener()
+}
+
+function mountFormListener() {
+    let songForm = document.getElementById('song-form')
+    console.log(songForm)
 }
--- frontend/src/user.js
+++ frontend/src/user.js
@@ -32,5 +32,5 @@ class User {
         }
         addSongButton()
     }
-    
+
 }
