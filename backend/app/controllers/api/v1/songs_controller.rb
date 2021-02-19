78c0895ffd8fd5436b7799859153d88b5b4d9028
kam3r0nz <kam.hungerford@gmail.com>
2021-02-19 14:39:22 -0600

    Created helper method in songs controller

--- backend/app/controllers/api/v1/songs_controller.rb
+++ backend/app/controllers/api/v1/songs_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::SongsController < ApplicationController
+    before_action :set_song, only: [:show, :destroy]
+    
     def index
         songs = Song.all
         render json: songs
     end
 
     def show
-        song = Song.find_by(id: params[:id])
-        if song
-            render json: song
+        if @song
+            render json: @song
         else
             render json: { Error: "That song does not exist." }
         end
@@ -19,12 +20,15 @@ class Api::V1::SongsController < ApplicationController
     end
 
     def destroy
-        song = Song.find_by(id: params[:id])
-        song.destroy
-        render json: song
+        @song.destroy
+        render json: @song
     end
 
     private
+
+    def set_song
+        @song = Song.find_by(id: params[:id])
+    end
 
     def song_params
         params.require(:song).permit(:title, :artist, :album, :album_cover, :user_id)
